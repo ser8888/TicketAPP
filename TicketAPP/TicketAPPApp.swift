efc2e31c64e2734996ba108c2e53a681356852e6
Sergejs Tiselskis <slokas85@gmail.con>
2022-11-02 22:38:43 +0200

    Initial Commit

---
+++ TicketAPP/TicketAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicketAPPApp.swift
+//  TicketAPP
+//
+//  Created by John Doe on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicketAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
