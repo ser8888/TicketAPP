00372874ea25b8c0ec2ad59e3e2c8abbf4d2522b
Sergejs Tiselskis <slokas85@gmail.con>
2022-11-03 16:38:55 +0200

    ticket details are ready

--- TicketAPP/Views/TicketView.swift
+++ TicketAPP/Views/TicketView.swift
@@ -10,27 +10,42 @@ import RealmSwift
 
 struct TicketView: View {
     @ObservedRealmObject var ticket: Ticket
+    
     var body: some View {
-        Text(ticket.title)
-    }
-}
-
-struct TicketView_Previews: PreviewProvider {
-    static var previews: some View {
-        
-        let ticket1 = Ticket(product: "Realm", title: "Ticket 1", details: "This is a big deal", author: "Andrew", created: Date(timeIntervalSinceReferenceDate: Date().timeIntervalSinceReferenceDate - 1000000), status: .notStarted)
-        
-        let ticket2 = Ticket(product: "Realm", title: "My second issue", details: "Всякие пустяки короче ", author: "Бузова", created: Date(timeIntervalSinceReferenceDate: Date().timeIntervalSinceReferenceDate - 1000000), status: .inProgress)
-      
-        let ticket3 = Ticket(product: "Realm", title: "Issue three", details: "This is not such a big deal. But I do want to talk about it at greate length. This is not such a big deal. But I do want to talk about it at greate length.", author: "Jane")
-        
-        return NavigationView {
-            List {
-                TicketView(ticket: ticket1)
-                TicketView(ticket: ticket2)
-                TicketView(ticket: ticket3)
+        VStack(alignment: .leading) {
+            HStack {
+                Text(ticket.title)
+                    .font(.headline)
+                    .foregroundColor(ticket.color)
+                Spacer()
+                DateView(date: ticket.created)
+                    .font(.caption)
             }
+            Text(ticket.author)
+                .font(.caption)
+                .foregroundColor(.secondary)
+            Text(ticket.details ?? "No details")
+                .font(.caption)
         }
     }
 }
+    struct TicketView_Previews: PreviewProvider {
+        static var previews: some View {
+            
+            let ticket1 = Ticket(product: "Realm", title: "Ticket 1", details: "This is a big deal", author: "Andrew", created: Date(timeIntervalSinceReferenceDate: Date().timeIntervalSinceReferenceDate - 1000000), status: .notStarted)
+            
+            let ticket2 = Ticket(product: "Realm", title: "My second issue", details: "Всякие пустяки короче ", author: "Бузова", created: Date(timeIntervalSinceReferenceDate: Date().timeIntervalSinceReferenceDate - 1000000), status: .inProgress)
+            
+            let ticket3 = Ticket(product: "Realm", title: "Issue three", details: "This is not such a big deal. But I do want to talk about it at greate length. This is not such a big deal. But I do want to talk about it at greate length.", author: "Jane", status: .complete)
+            
+            return NavigationView {
+                List {
+                    TicketView(ticket: ticket1)
+                    TicketView(ticket: ticket2)
+                    TicketView(ticket: ticket3)
+                }
+            }
+        }
+    }
+    
 
